fecd7b224b253dc9e700608e35f602c4ac3cc944
Michelle Jowers <michelle.jowers@icloud.com>
2021-07-12 09:00:09 -0400

    updating file

--- Guessing_Game/Ch05_prj3_Guessing_Game/Program.cs
+++ Guessing_Game/Ch05_prj3_Guessing_Game/Program.cs
@@ -3,6 +3,9 @@
 namespace Ch05_prj3_Guessing_Game {
     class Program {
         static void Main(string[] args) {
+
+           
+
             Console.WriteLine("Welcome to the Guessing Game!");
             Console.WriteLine("+++++++++++++++++++++++++++++");
 
@@ -17,39 +20,39 @@ namespace Ch05_prj3_Guessing_Game {
 
                 Random rand = new Random();
                 decimal num = rand.Next(1, 101);
-                 while (guess != num) {
-                        if (guess < (num - 10)) {
-                            Console.WriteLine("Way too Low!");
-                        } if (guess > (num + 10)) {
-                            Console.WriteLine("Way too High!");
-                        } if (guess > num) {
-                            Console.WriteLine("Too High");
-                        } if (guess < num) {
-                            Console.WriteLine("Too Low");
-                       
-                    Console.WriteLine($"Try Again (y/n)? ");
-                    choice = Console.ReadLine().ToLower();
-                    Console.WriteLine();
-                }
-                
-
+                while (guess != num) {
+                    if (guess < (num - 9)) {
+                        Console.WriteLine("Way too Low!");
+                    } else
+                    if (guess > (num + 9)) {
+                        Console.WriteLine("Way too High!");
+                    } else
+                    if (guess > num) {
+                        Console.WriteLine("Too High");
+                    } else
+                    if (guess < num) {
+                        Console.WriteLine("Too Low");
+                    }
+                    
+                        Console.WriteLine();
+                        Console.Write($"Try Again (y/n)? ");
+                        choice = Console.ReadLine();
+                    }
+                } 
                 while (guess == num) {
                     Console.WriteLine($"You guess it in {count} tries!");
-                    if (count <= 3) {
-                        Console.WriteLine($"\nGreat Work! You are a mathematical wizard!");
-                    }
-                    if (count > 3 && count <= 7) {
-                        Console.WriteLine($"\nNot too bad! You have potential!");
-                    }
-                    if (count > 7) {
-                        Console.WriteLine($"\n What took you so long? " +
-                                          $"You might want to take some lessons.");
-                    }
-                 
+                } if (count <= 3) {
+                    Console.WriteLine($"\nGreat Work! You are a mathematical wizard!");
+                } else if (count > 3 && count <= 7) {
+                    Console.WriteLine($"\nNot too bad! You have potential!");
+                } else if (count > 7) {
+                    Console.WriteLine($"\n What took you so long? " +
+                                      $"You might want to take some lessons.");
                 }
-          
-            }
+            }   
         }
     }
 }
+
+
 
